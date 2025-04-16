deb4dda45fe12141a210dcf73e9ec63b746cfdc7
zaiimrq <zaiimrq@gmail.com>
2025-04-17 06:44:31 +0800

    feat(database): add Quiz

---
+++ app/Models/Quiz.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    /** @use HasFactory<\Database\Factories\QuizFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'slug',
+        'name',
+        'description',
+    ];
+}
