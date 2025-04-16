83b0e9d9d273683de354ed14a17adbf603d1c735
zaiimrq <zaiimrq@gmail.com>
2025-04-17 07:34:13 +0800

    feat(provider): configure db, date, and model settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureDB();
+        $this->configureDates();
+        $this->configureVites();
+        $this->configureModels();
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(! app()->isProduction());
+        Model::automaticallyEagerLoadRelationships();
+    }
+
+    private function configureDB(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    private function configureVites(): void
+    {
+        Vite::useWaterfallPrefetching();
     }
 }
