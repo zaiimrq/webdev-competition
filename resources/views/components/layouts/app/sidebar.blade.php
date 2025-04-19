fc3d6125af52ec4f5aa3ea860e5db331d30cdb76
zaiimrq <zaiimrq@gmail.com>
2025-04-19 21:12:54 +0900

    fix(sidebar): update quiz icon

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -14,7 +14,7 @@
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
-                    <flux:navlist.item icon="home" :href="route('quizzes')" :current="request()->is('quizzes*')" wire:navigate>{{ __('Quiz') }}</flux:navlist.item>
+                    <flux:navlist.item icon="light-bulb" :href="route('quizzes')" :current="request()->is('quizzes*')" wire:navigate>{{ __('Quiz') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
